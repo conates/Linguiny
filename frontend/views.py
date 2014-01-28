77e9064d6b44a75b48ae0429279fbf68ad123527
conates <conates.ktn@gmail.com>
2014-01-27 23:41:19 -0300

    Maquetación basica home, add templatetags, add class form

--- frontend/views.py
+++ frontend/views.py
@@ -1,8 +1,34 @@
 # -*- encoding: utf-8 -*-
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404,redirect
 from django.template.response import TemplateResponse
+from frontend.forms import SearchForm
+from frontend.models import Profile
+from django.contrib.auth.models import User
+
 
 # Create your views here.
 def home(request):
-	return TemplateResponse(request, 'home.html')
-	
+
+	form = SearchForm()
+	return render(request, 'home.html', {'form': form})
+
+
+
+
+def results(request):
+	translate_from = request.POST.get('translate_from')
+	translate_to = request.POST.get('translate_to')
+	results = Profile.objects.filter(translate_from=translate_from,translate_to=translate_to)
+
+
+
+	return render(request, 'results.html',{'results':results,'translate_from':translate_from,'translate_to':translate_to})
+
+
+
+
+def profile(request, profile_id):
+
+	profile = get_object_or_404(Profile, pk=profile_id)
+
+	return render(request, 'profile.html',{'profile':profile})
